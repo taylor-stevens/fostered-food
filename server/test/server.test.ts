ba8cbde2db077d27da8bb5a0ee60182482030e13
Matt-Blanco <mattmblanco@gmail.com>
2022-05-16 19:50:39 -0400

    finalized pulling fridge info from multiple sources

---
+++ server/test/server.test.ts
@@ -0,0 +1,7 @@
+const fridgeInfo = require('../src/server');
+
+describe('Tests for pulling fridge information from database', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(1 + 2).toBe(3);
+      });
+})
