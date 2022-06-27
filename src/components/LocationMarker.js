db06af945493cf3330397083e92cbec5e739315f
Taylor Stevens <taylorstevens14562@gmail.com>
2022-06-27 18:04:48 -0400

    updated the descriptions of the components in the map

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -7,10 +7,10 @@ import LocationPopup from "./LocationPopup";
 
 /**
  * A marking on the Map representing the location of a community fridge.
- * @param fridge - A single community fridge from the fridges JSON file.
- * @param selectedFridge - The current fridge on the map that is selected, if there is one.
- * @param updateSelected - the function that updates the current selected fridge on the map to a new one.
- * @returns {JSX.Element} A Marker with a popup that describes the given fridge
+ * @param fridge {JSON} - A single community fridge from the fridges JSON file.
+ * @param selectedFridge {JSON} - The current fridge on the map that is selected, if there is one.
+ * @param updateSelected {hook} - the function that updates the current selected fridge on the map to a new one.
+ * @returns {JSX.Element} - A Marker with a popup that describes the given fridge
  */
 export default function LocationMarker({fridge, selectedFridge, updateSelected}) {
     
